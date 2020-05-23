b845530de7c0267a7b63264ae38ee3738d719d90
XMUlinhui <53165595+XMUlinhui@users.noreply.github.com>
2020-05-23 17:38:03 +0800

    Add files via upload

--- name.php
+++ name.php
@@ -1,6 +1,6 @@
 <?php
     header('Content-Type: text/html; charset=gb2312');
-	$command = 'Project1.exe '.$_POST['name'].' '.$_POST['pwd'].' '.$_POST['serial'];
+	$command = 'CNproject3-php.exe '.$_POST['name'].' '.$_POST['pwd'].' '.$_POST['serial'];
     $result = passthru($command);
     print_r($result);
 ?>
